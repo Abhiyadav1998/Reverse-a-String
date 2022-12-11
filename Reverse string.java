c878fed4605766a0844b3089d36c25697179ead2
Abhiyadav1998 <106450934+Abhiyadav1998@users.noreply.github.com>
2022-12-12 00:31:39 +0530

    Add files via upload

---
+++ Reverse string.java
@@ -0,0 +1,11 @@
+package app_1;
+
+public class A {
+	public static void main(String[] args) {
+		String str="Pankaj Sir Academy";
+		for(int i=str.length()-1; i>=0; i--) {
+			System.out.print(str.charAt(i));
+		}
+	}
+
+}
